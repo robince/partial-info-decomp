92fc6fbfa39a6ca56d92427af1acad5a94fb92b6
Robin Ince <robince@gmail.com>
2016-01-25 17:17:54 +0000

    new measure vs Imin PID on examples from the literature

---
+++ examples_2d.m
@@ -0,0 +1,222 @@
+% two variable examples
+
+%% W&B FIGURE 4A
+Pxxy = zeros(2,2,3);
+Pxxy(1,1,1) = 1/3;
+Pxxy(1,2,2) = 1/3;
+Pxxy(2,1,3) = 1/3;
+
+fprintf(1,'Williams and Beer Fig 4A\n')
+compare(lattice2d(),Pxxy);
+
+%% W&B FIGURE 4B
+Pxxy = zeros(2,2,3);
+Pxxy(1,1,1) = 0.25;
+Pxxy(1,2,2) = 0.25;
+Pxxy(2,2,2) = 0.25;
+Pxxy(2,1,3) = 0.25;
+
+fprintf(1,'Williams and Beer Fig 4B\n')
+compare(lattice2d(),Pxxy);
+
+%% W&B FIGURE 4 MODIFIED C
+Pxxy = zeros(2,2,3);
+p = 1/6;
+Pxxy(1,1,1) = p;
+Pxxy(1,2,2) = p'
+Pxxy(2,2,2) = p;
+Pxxy(2,1,3) = p;
+Pxxy(2,2,1) = p;
+Pxxy(1,2,3) = p;
+
+fprintf(1,'Williams and Beer Fig 4 Modified\n')
+compare(lattice2d(),Pxxy);
+
+%% W&B FIGURE 4 MODIFIED D
+Pxxy = zeros(2,2,3);
+p = 1/5;
+Pxxy(1,1,1) = p;
+Pxxy(1,2,2) = p'
+Pxxy(2,2,2) = p;
+Pxxy(2,1,3) = p;
+Pxxy(2,2,1) = p;
+% Pxxy(1,2,3) = p;
+
+fprintf(1,'Williams and Beer Fig 4 Modified\n')
+compare(lattice2d(),Pxxy);
+
+%% one bit copy
+Pxxy = zeros(2,2,2);
+Pxxy(1,1,1) = 0.25;
+Pxxy(1,2,1) = 0.25;
+Pxxy(2,1,2) = 0.25;
+Pxxy(2,2,2) = 0.25;
+
+fprintf(1,'1 bit copy\n')
+compare(lattice2d(),Pxxy);
+
+%% two bit copy (independent bits)
+% UNQ : Griffith et al. (2014) Fig. 1
+% UNQ : Griffith et al. (2012) Fig. 3
+Pxxy = zeros(2,2,4);
+Pxxy(1,1,1) = 0.25;
+Pxxy(1,2,2) = 0.25;
+Pxxy(2,1,3) = 0.25;
+Pxxy(2,2,4) = 0.25;
+
+fprintf(1,'2 bit copy (independent)\n')
+compare(lattice2d(),Pxxy);
+
+%% 2 bit copy (redundant bits)
+Pxxy = zeros(2,2,4);
+Pxxy(1,1,1) = 0.25;
+Pxxy(1,1,2) = 0.25;
+Pxxy(2,2,3) = 0.25;
+Pxxy(2,2,4) = 0.25;
+fprintf(1,'2 bit copy (redundant)\n')
+compare(lattice2d(),Pxxy);
+
+%% Unequal independent copy
+Pxxy = zeros(4,2,8);
+p = 1/8;
+Pxxy(1,1,1) = p;
+Pxxy(2,1,2) = p;
+Pxxy(3,1,3) = p;
+Pxxy(4,1,4) = p;
+Pxxy(1,2,5) = p;
+Pxxy(2,2,6) = p;
+Pxxy(3,2,7) = p;
+Pxxy(4,2,8) = p;
+
+fprintf(1,'Unequal independent copy\n')
+compare(lattice2d(),Pxxy);
+
+%% OR
+Pxxy = zeros(2,2,2);
+Pxxy(1,1,1) = 0.25;
+Pxxy(1,2,2) = 0.25;
+Pxxy(2,1,2) = 0.25;
+Pxxy(2,2,2) = 0.25;
+fprintf(1,'OR\n')
+compare(lattice2d(),Pxxy);
+
+% lat = lattice2d();
+% lat = calc_pi_ri(lat,Pxxy,@Iccs);
+% latmin = calc_pi_wb(lat,Pxxy,@Imin);
+%% XOR
+% Griffith et al. (2012) Fig 4
+Pxxy = zeros(2,2,2);
+Pxxy(1,1,1) = 0.25;
+Pxxy(1,2,2) = 0.25;
+Pxxy(2,1,2) = 0.25;
+Pxxy(2,2,1) = 0.25;
+fprintf(1,'XOR\n')
+compare(lattice2d(),Pxxy);
+
+%% AND
+Pxxy = zeros(2,2,2);
+Pxxy(1,1,1) = 0.25;
+Pxxy(1,2,1) = 0.25;
+Pxxy(2,1,1) = 0.25;
+Pxxy(2,2,2) = 0.25;
+fprintf(1,'AND\n')
+compare(lattice2d(),Pxxy);
+
+%% RNDXOR : Griffith et al. (2014) Fig. 2
+% Griffith et al. (2012) Fig. 8 
+Pxxy = zeros(4,4,4);
+p = 1/8;
+Pxxy(1,1,1) = p;
+Pxxy(1,2,2) = p;
+Pxxy(2,1,2) = p;
+Pxxy(2,2,1) = p;
+Pxxy(3,3,3) = p;
+Pxxy(3,4,4) = p;
+Pxxy(4,3,4) = p;
+Pxxy(4,4,3) = p;
+
+fprintf(1,'RNDXOR Griffith (2014) Fig 2\n');
+compare(lattice2d(),Pxxy);
+
+lat = lattice2d();
+lat = calc_pi_ri(lat,Pxxy,@Iccs);
+latmin = calc_pi_wb(lat,Pxxy,@Imin);
+
+%% IMPERFECTRDN : Griffith et al. (2014) Fig. 3
+Pxxy = zeros(2,2,2);
+% Pxxy(1,1,1) = 0.4;
+% Pxxy(1,2,1) = 0.1;
+% Pxxy(2,2,2) = 0.5;
+Pxxy(1,1,1) = 0.499;
+Pxxy(1,2,1) = 0.001;
+Pxxy(2,2,2) = 0.5;
+
+fprintf(1,'IMPERFECTRDN Griffith (2014) Fig 3\n');
+compare(lattice2d(),Pxxy);
+% NB: shows higher {1}{2} than Imin! This is because {2} has unique
+% misinformation
+
+%% SUBTLE : Griffith et al. (2014) Fig. 4
+Pxxy = zeros(2,2,4);
+Pxxy(1,1,1) = 1/3;
+Pxxy(1,2,2) = 1/3;
+Pxxy(2,2,4) = 1/3;
+
+fprintf(1,'SUBTLE Griffith (2014) Fig 4\n');
+compare(lattice2d(),Pxxy);
+
+%% RDN : Griffith et al. (2012) Fig. 2
+Pxxy = zeros(2,2,2);
+Pxxy(1,1,1) = 0.5;
+Pxxy(2,2,2) = 0.5;
+
+fprintf(1,'RDN Griffith (2012) Fig 2\n');
+compare(lattice2d(),Pxxy);
+
+%% RDNUNQXOR : Griffith et al. (2012) Fig 12
+Pxxy = zeros(8,8,16);
+p = 1./32;
+Pxxy(1,1,1) = p;
+Pxxy(1,2,2) = p;
+Pxxy(2,1,2) = p;
+Pxxy(2,2,1) = p;
+
+Pxxy(1,3,3) = p;
+Pxxy(1,4,4) = p;
+Pxxy(2,3,4) = p;
+Pxxy(2,4,3) = p;
+
+Pxxy(3,1,5) = p;
+Pxxy(3,2,6) = p;
+Pxxy(4,1,6) = p;
+Pxxy(4,2,5) = p;
+
+Pxxy(3,3,7) = p;
+Pxxy(3,4,8) = p;
+Pxxy(4,3,8) = p;
+Pxxy(4,4,7) = p;
+
+
+
+Pxxy(5,5,9) = p;
+Pxxy(5,6,10) = p;
+Pxxy(6,5,10) = p;
+Pxxy(6,6,9) = p;
+
+Pxxy(5,7,11) = p;
+Pxxy(5,8,12) = p;
+Pxxy(6,7,12) = p;
+Pxxy(6,8,11) = p;
+
+Pxxy(7,5,13) = p;
+Pxxy(7,6,14) = p;
+Pxxy(8,5,14) = p;
+Pxxy(8,6,13) = p;
+
+Pxxy(7,7,15) = p;
+Pxxy(7,8,16) = p;
+Pxxy(8,7,16) = p;
+Pxxy(8,8,15) = p;
+
+fprintf(1,'RDNUNQXOR Griffith (2012) Fig 2\n');
+compare(lattice2d(),Pxxy);
---
+++ examples_3d.m
@@ -0,0 +1,128 @@
+% three variable examples
+%% 3 way redundant
+Pxxxy = zeros(2,2,2,2);
+Pxxxy(1,1,1,1) = 1/2;
+Pxxxy(2,2,2,2) = 1/2;
+
+fprintf(1,'RDN: redundant bits\n')
+compare(lattice3d(),Pxxxy);
+
+%% XORDUPLICATE : Griffith et al. (2012) Fig. 5
+Pxxxy = zeros(2,2,2,2);
+p = 1/4;
+Pxxxy(1,1,1,1) = p;
+Pxxxy(1,2,1,2) = p;
+Pxxxy(2,1,2,2) = p;
+Pxxxy(2,2,2,1) = p;
+
+fprintf(1,'XORDUPLICATE Griffith at al. (2012) Fig. 5\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% two separate xors
+Pxxxy = zeros(2,2,2,4);
+
+p = 1/8;
+Pxxxy(1,1,1,1) = p;
+Pxxxy(1,1,2,2) = p;
+Pxxxy(1,2,1,4) = p;
+Pxxxy(1,2,2,3) = p;
+
+Pxxxy(2,1,1,3) = p;
+Pxxxy(2,1,2,4) = p;
+Pxxxy(2,2,1,2) = p;
+Pxxxy(2,2,2,1) = p;
+
+fprintf(1,'Two separate Xors\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% XORLOSES : Griffith et al. (2012) Fig. 6
+Pxxxy = zeros(2,2,2,2);
+Pxxxy(1,1,1,1) = 1/4;
+Pxxxy(1,2,2,2) = 1/4;
+Pxxxy(2,1,2,2) = 1/4;
+Pxxxy(2,2,1,1) = 1/4;
+
+fprintf(1,'XORLOSSES Griffith at al. (2012) Fig. 6\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% ANDDUPLICATE : Griffith et al. (2012) Fig. 10
+Pxxxy = zeros(2,2,2,2);
+p = 1/4;
+Pxxxy(1,1,1,1) = p;
+Pxxxy(1,2,1,1) = p;
+Pxxxy(2,1,2,1) = p;
+Pxxxy(2,2,2,2) = p;
+
+fprintf(1,'XORDUPLICATE Griffith at al. (2012) Fig. 5\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% 2 bit xor, with independent extra bit
+Pxxxy = zeros(2,2,2,4);
+
+p = 1/8;
+Pxxxy(1,1,1,1) = p;
+Pxxxy(1,1,2,2) = p;
+Pxxxy(1,2,1,3) = p;
+Pxxxy(1,2,2,4) = p;
+Pxxxy(2,1,1,3) = p;
+Pxxxy(2,1,2,4) = p;
+Pxxxy(2,2,1,1) = p;
+Pxxxy(2,2,2,2) = p;
+
+fprintf(1,'2 bit XOR with independent extra bit\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% Bertschinger / Rauh. (x1,x2,x1 XOR x2) copied to output
+Pxxxy = zeros(2,2,2,8);
+Pxxxy(1,1,1,1) = 0.25;
+Pxxxy(1,2,2,4) = 0.25;
+Pxxxy(2,1,2,6) = 0.25;
+Pxxxy(2,2,1,7) = 0.25;
+
+fprintf(1,'Bertschinger / Rauh XOR copied input\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% 3 way XOR
+Pxxxy = zeros(2,2,2,2);
+Pxxxy(1,1,1,1) = 1/8;
+Pxxxy(1,1,2,2) = 1/8;
+Pxxxy(1,2,1,2) = 1/8;
+Pxxxy(1,2,2,1) = 1/8;
+Pxxxy(2,1,1,2) = 1/8;
+Pxxxy(2,1,2,1) = 1/8;
+Pxxxy(2,2,1,1) = 1/8;
+Pxxxy(2,2,2,1) = 1/8;
+
+fprintf(1,'3-way XOR\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% 3 way AND
+Pxxxy = zeros(2,2,2,2);
+Pxxxy(1,1,1,1) = 1/8;
+Pxxxy(1,1,2,1) = 1/8;
+Pxxxy(1,2,1,1) = 1/8;
+Pxxxy(1,2,2,1) = 1/8;
+Pxxxy(2,1,1,1) = 1/8;
+Pxxxy(2,1,2,1) = 1/8;
+Pxxxy(2,2,1,1) = 1/8;
+Pxxxy(2,2,2,2) = 1/8;
+
+fprintf(1,'3-way AND\n')
+lat = compare(lattice3d(),Pxxxy);
+
+%% XORMULTICOAL : Griffith et al. (2012) Fig. 11
+Pxxxy = zeros(4,4,4,2);
+p = 1/8;
+Pxxxy(1,1,1,1) = p;
+Pxxxy(4,3,3,1) = p;
+Pxxxy(3,4,2,1) = p;
+Pxxxy(2,2,4,1) = p;
+
+Pxxxy(3,3,1,2) = p;
+Pxxxy(2,1,3,2) = p;
+Pxxxy(1,2,2,2) = p;
+Pxxxy(4,4,4,2) = p;
+
+fprintf(1,'XORMULTICOAL Griffith at al. (2012) Fig. 11\n')
+lat = compare(lattice3d(),Pxxxy);
+

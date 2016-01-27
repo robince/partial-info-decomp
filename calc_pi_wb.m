95ae7da28485603f1ca343763cde0024a87e525c
Robin Ince <robince@gmail.com>
2016-01-27 12:42:39 +0000

    simplify W&B lattice summation (remove unnecessary recursion)

--- calc_pi_wb.m
+++ calc_pi_wb.m
@@ -18,30 +18,26 @@ end
 % use equation (7) from Williams and Beer to calculate
 % PI at each node
 lat.PI = NaN(size(lat.Icap));
-% start at the bottom
-ni = lat.bottom;
-lat.PI(ni) = lat.Icap(ni);
-% recurse up the tree
-lat = recurse_calc_parents(lat,ni);
 
-
-
-function lat = recurse_calc_parents(lat,ni)
-% calculate all parents of this node
-lat = calc_parents(lat,ni);
-% recurse up
-for pi=lat.parents{ni}
-    lat = recurse_calc_parents(lat,pi);
+% ascend through levels of the lattice
+Nlevels = max(lat.level);
+for li=1:Nlevels
+    nodes = find(lat.level==li);
+    for ni=nodes
+        lat = calc_pi_node(lat,ni);
+    end
 end
 
 
 
-function lat = calc_parents(lat,ni)
-for pi=lat.parents{ni}
-    all_children = unique(recurse_children(lat,pi,[]));
-    thsPI = lat.Icap(pi) - sum(lat.PI(all_children));
-    lat.PI(pi) = thsPI;
+function lat = calc_pi_node(lat,ni)
+all_children = recurse_children(lat,ni,[]);
+if isempty(all_children)
+    thsPI = lat.Icap(ni);
+else
+    thsPI = lat.Icap(ni) - sum(lat.PI(all_children));
 end
+lat.PI(ni) = thsPI;
 
 
 
@@ -50,4 +46,5 @@ children = [children lat.children{ni}];
 for ci=lat.children{ni}
     children = recurse_children(lat,ci,children);
 end
+children = unique(children);
 

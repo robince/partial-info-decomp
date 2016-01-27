fda607c4b9c26bd71fa2f18510563c4a75b4b454
Robin Ince <robince@gmail.com>
2016-01-27 15:35:36 +0000

    fix display for numerical error

--- display_nonzero_terms.m
+++ display_nonzero_terms.m
@@ -1,6 +1,6 @@
 function display_nonzero_terms(lat)
 
-terms = lat.labels(lat.PI>0);
+terms = lat.labels(lat.PI>2*eps(lat.PI));
 str = sprintf('%s  ',terms{:});
 fprintf(1,'NZ PI Terms: %s\n',str);
 % fprintf(1,'\n')

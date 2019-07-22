004e9068da1e6b5f13d18c699089da89dc372af1
Robin Ince <robince@gmail.com>
2019-07-22 11:31:56 +0100

    add basic sampling utility function to calculate discrete Iccs PID from data

---
+++ calcpid.m
@@ -0,0 +1,36 @@
+function pid = calcpid(SA, Na, SB, Nb, T, Nt, Ntrl)
+% Sample probablity distributions from discrete data and use them 
+% to calculate a PID with Iccs
+% SA - source A (integer values in range [0, Na-1]
+% Na - number of bins for SA
+% SB - source B (integer values in range [0, Nb-1]
+% Nb - number of bins for SB
+% T  - target (integer values in range [0, Nt-1]
+% Ntrl - number of trials
+
+SA = SA(:);
+SB = SB(:);
+T = T(:);
+
+if length(SA)~=Ntrl || length(SB)~=Ntrl || length(T)~=Ntrl
+    error('calcpid: wrong number of trials in data')
+end
+if any(SA<0) || any(SA>Na-1)
+    error('calcpid: problem with SA input')
+end
+if any(SB<0) || any(SB>Nb-1)
+    error('calcpid: problem with SB input')
+end
+if any(T<0) || any(T>Nt-1)
+    error('calcpid: problem with T input')
+end
+
+Sjoint = T*(Nb*Na) + SB*Na + SA;
+Nj = Na*Nb*Nt;
+
+P = prob(Sjoint, Nj);
+P = reshape(P, [Na Nb Nt]);
+
+% compute PID
+lat = calc_pi(lattice2d(),P,@Iccs);
+pid = lat.PI;
---
+++ prob.m
@@ -0,0 +1,10 @@
+function Pr=prob(data, nR)
+
+PrPos	= zeros(1,nR-1);
+% diff of find of diff trick for counting number of elements
+temp 	= sort(data(data>0));	% want vector excluding P(0)
+dtemp	= diff([temp;max(temp)+1]);
+count 	= diff(find([1;dtemp]));
+indx 	= temp(dtemp>0);
+PrPos(indx)= count ./ numel(data);	% probability
+Pr = [1-sum(PrPos)  PrPos];
